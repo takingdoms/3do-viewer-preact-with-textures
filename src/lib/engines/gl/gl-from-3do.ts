9581a49dcaa05f5bcf9d73f61a078f5c9b34161d
C L <c1000@protonmail.com>
2023-02-07 20:34:31 -0300

    remade the original pyramid (flat colors) using the model helper

--- src/lib/engines/gl/gl-from-3do.ts
+++ src/lib/engines/gl/gl-from-3do.ts
@@ -22,27 +22,31 @@ function glModelFrom3do(object3do: Object3do, ctx: GlContext): GlModel {
   const v3: Vector3 = [+1,  0, +1];
   const v4: Vector3 = [ 0, +1,  0];
 
-  const white: ColorRGBA = [1.0, 1.0, 1.0, 1.0];
+  const wht: ColorRGBA = [1.0, 1.0, 1.0, 1.0];
   const red: ColorRGBA = [1.0, 0.0, 0.0, 1.0];
-  const green: ColorRGBA = [0.0, 1.0, 0.0, 1.0];
-  const blue: ColorRGBA = [0.0, 0.0, 1.0, 1.0];
-  const yellow: ColorRGBA = [1.0, 1.0, 0.0, 1.0];
+  const gre: ColorRGBA = [0.0, 1.0, 0.0, 1.0];
+  const blu: ColorRGBA = [0.0, 0.0, 1.0, 1.0];
+  const yel: ColorRGBA = [1.0, 1.0, 0.0, 1.0];
 
   return GlModelHelpers.createModelFromIndexedColoredVertices(ctx, {
     vertices: [
-      { vertex: v0, color: blue },
-      { vertex: v1, color: red },
-      { vertex: v2, color: green },
-      { vertex: v3, color: yellow },
-      { vertex: v4, color: white },
+      { vertex: v0, color: blu }, { vertex: v1, color: blu }, { vertex: v4, color: blu }, // 0, 1, 2
+      { vertex: v1, color: red }, { vertex: v2, color: red }, { vertex: v4, color: red }, // 3, 4, 5
+      { vertex: v2, color: gre }, { vertex: v3, color: gre }, { vertex: v4, color: gre }, // 6, 7, 8
+      { vertex: v0, color: yel }, { vertex: v3, color: yel }, { vertex: v4, color: yel }, // 9, 10, 11
+
+      { vertex: v0, color: wht }, // 12
+      { vertex: v1, color: wht }, // 13
+      { vertex: v2, color: wht }, // 14
+      { vertex: v3, color: wht }, // 15
     ],
     indices: [
-      0, 1, 4,
-      1, 2, 4,
-      2, 3, 4,
-      0, 3, 4,
       0, 1, 2,
-      0, 2, 3,
+      3, 4, 5,
+      6, 7, 8,
+      9, 10, 11,
+      12, 13, 14,
+      12, 14, 15,
     ],
   });
 }
