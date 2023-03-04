2377dd55aa3d099ed45af101ea9ee79c971f8684
C L <c1000@protonmail.com>
2023-03-04 14:19:00 -0300

    Loader component now waits all images to load before rendering the Main component

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -6,74 +6,6 @@ import { WebglEngineShaderSources } from "../../lib/engines/webgl-engine";
 import { TextureMapping } from "../../lib/texture-mapping";
 import Main, { ObjectStateMap } from "../Main";
 
-async function loadShaders(): Promise<WebglEngineShaderSources> {
-  return {
-    normal: {
-      fsSource: await fetch('/assets/shaders/normal.fs').then((res) => res.text()),
-      vsSource: await fetch('/assets/shaders/normal.vs').then((res) => res.text()),
-    },
-    solid: {
-      fsSource: await fetch('/assets/shaders/solid_color.fs').then((res) => res.text()),
-      vsSource: await fetch('/assets/shaders/solid_color.vs').then((res) => res.text()),
-    },
-    wireframe: {
-      fsSource: await fetch('/assets/shaders/wireframe.fs').then((res) => res.text()),
-      vsSource: await fetch('/assets/shaders/wireframe.vs').then((res) => res.text()),
-    },
-  };
-}
-
-async function loadModel(dataSource: File | string): Promise<Object3doTree> {
-  let data: ArrayBuffer;
-
-  if (typeof dataSource === 'string') {
-    const response = await fetch(dataSource);
-    data = await response.arrayBuffer();
-  }
-  else {
-    data = await dataSource.arrayBuffer();
-  }
-
-  const view = new DataView(data);
-  return Parse3do.fromBuffer(view);
-}
-
-async function loadTextures(tree: Object3doTree): Promise<TextureMapping> {
-  const textureNames = new Set<string>();
-
-  const loadNext = (node: Object3do) => {
-    for (const primitive of node.primitives) {
-      textureNames.add(primitive.textureName);
-    }
-
-    for (const child of node.children) {
-      loadNext(child);
-    }
-  };
-
-  for (const rootNode of tree.rootNodes) {
-    loadNext(rootNode);
-  }
-
-  const result: TextureMapping = {};
-
-  for (let textureName of textureNames) {
-    textureName = textureName.trim();
-
-    if (textureName === '') {
-      result[textureName] = null;
-      continue;
-    }
-
-    const image = new Image();
-    image.src = `/assets/custom/pngs/${textureName}.png`;
-
-    result[textureName] = { type: 'html', image };
-  }
-
-  return result;
-}
-
 const Loader: FunctionComponent<{
   dataSource: File | string; // string = url
 }> = ({ dataSource }) => {
@@ -158,6 +90,86 @@ function createStateMap(object3doTree: Object3doTree): ObjectStateMap {
   for (const rootChild of object3doTree.rootNodes) {
     next(rootChild);
   }
+
+  return result;
+}
+
+async function loadShaders(): Promise<WebglEngineShaderSources> {
+  return {
+    normal: {
+      fsSource: await fetch('/assets/shaders/normal.fs').then((res) => res.text()),
+      vsSource: await fetch('/assets/shaders/normal.vs').then((res) => res.text()),
+    },
+    solid: {
+      fsSource: await fetch('/assets/shaders/solid_color.fs').then((res) => res.text()),
+      vsSource: await fetch('/assets/shaders/solid_color.vs').then((res) => res.text()),
+    },
+    wireframe: {
+      fsSource: await fetch('/assets/shaders/wireframe.fs').then((res) => res.text()),
+      vsSource: await fetch('/assets/shaders/wireframe.vs').then((res) => res.text()),
+    },
+  };
+}
+
+async function loadModel(dataSource: File | string): Promise<Object3doTree> {
+  let data: ArrayBuffer;
+
+  if (typeof dataSource === 'string') {
+    const response = await fetch(dataSource);
+    data = await response.arrayBuffer();
+  }
+  else {
+    data = await dataSource.arrayBuffer();
+  }
+
+  const view = new DataView(data);
+  return Parse3do.fromBuffer(view);
+}
+
+async function loadTextures(tree: Object3doTree): Promise<TextureMapping> {
+  const textureNames = new Set<string>();
+
+  const loadNext = (node: Object3do) => {
+    for (const primitive of node.primitives) {
+      textureNames.add(primitive.textureName);
+    }
+
+    for (const child of node.children) {
+      loadNext(child);
+    }
+  };
+
+  for (const rootNode of tree.rootNodes) {
+    loadNext(rootNode);
+  }
+
+  const result: TextureMapping = {};
+
+  await Promise.all(Array.from(textureNames).map((textureName) => {
+    return new Promise<void>((resolve, reject) => {
+      textureName = textureName.trim();
+
+      if (textureName === '') {
+        result[textureName] = null;
+        resolve();
+        return;
+      }
+
+      const image = new Image();
+      image.src = `/assets/custom/pngs/${textureName}.png`;
+
+      image.onerror = () => {
+        console.error(`Failed to load image at: ${image.src}`);
+        result[textureName] = null;
+        resolve();
+      };
+
+      image.onload = () => {
+        result[textureName] = { type: 'html', image };
+        resolve();
+      };
+    });
+  }));
 
   return result;
 }
